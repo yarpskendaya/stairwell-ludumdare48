e797d09452373aa59b54a6270d31870a399df75c
yarpskendaya <jangohemmes@hotmail.com>
2021-04-25 14:16:08 +0200

    Finish soulpusher and add facadescreen

--- src/scripts/souls/soul-pusher.js
+++ src/scripts/souls/soul-pusher.js
@@ -1,10 +1,5 @@
 var SoulPusher = pc.createScript('soulPusher');
 
-SoulPusher.attributes.add('tile', {
-    type: 'vec2',
-    description: 'The tile this pusher affects'
-});
-
 SoulPusher.prototype.direction = new pc.Vec2();
 SoulPusher.prototype.affectedTile = new pc.Vec2();
 
@@ -17,14 +12,14 @@ SoulPusher.prototype.initialize = function () {
 };
 
 SoulPusher.prototype.getDirection = function () {
-    var rotation = this.entity.getEulerAngles().y % 360;
-    if (this.isAlmostEqual(rotation, 0, 360))
+    var rotation = this.entity.getEulerAngles();
+    if (this.isNotRotated(rotation))
         return new pc.Vec2(1, 0);
-    if (this.isAlmostEqual(rotation, 90, -270))
+    if (this.isQuarterRotated(rotation))
         return new pc.Vec2(0, 1);
-    if (this.isAlmostEqual(rotation, 180, -180))
+    if (this.isHalfRotated(rotation))
         return new pc.Vec2(-1, 0);
-    if (this.isAlmostEqual(rotation, 270, -90))
+    if (this.isThreeQuartersRotated(rotation))
         return new pc.Vec2(0, -1);
 
     console.error('Could not get direction from soulpusher orientation');
@@ -39,30 +34,62 @@ SoulPusher.prototype.getAffectedTile = function () {
 };
 
 SoulPusher.prototype.getAffectedTileX = function (z) {
-    if (this.isAlmostEqual(z, -5))
+    if (this.isAlmostEqual(z, -2, -5))
         return 0;
     if (this.isAlmostEqual(z, 0))
         return 1;
-    if (this.isAlmostEqual(z, 5))
+    if (this.isAlmostEqual(z, 2, 5))
         return 2;
 };
 
 SoulPusher.prototype.getAffectedTileY = function (x) {
-    if (this.isAlmostEqual(x, -5))
+    if (this.isAlmostEqual(x, -2, -5))
         return 0;
     if (this.isAlmostEqual(x, 0))
         return 1;
-    if (this.isAlmostEqual(x, 5))
+    if (this.isAlmostEqual(x, 2, 5))
         return 2;
 };
 
 SoulPusher.prototype.onClick = function () {
     var y = this.entity.getPosition().y;
     console.debug('Pushing at height ' + y);
-    this.app.fire('push-souls', this.tile, y, this.direction);
+    this.app.fire('push-souls', this.affectedTile, y, this.direction);
 };
 
-SoulPusher.prototype.isAlmostEqual = function(x, a, b) {
+SoulPusher.prototype.isNotRotated = function (rotation) {
+    var y = rotation.y % 360;
+    return this.isAlmostEqual(y, 0) &&
+        this.isAlmostEqual(rotation.x, 0) &&
+        this.isAlmostEqual(rotation.z, 0)
+};
+
+SoulPusher.prototype.isQuarterRotated = function (rotation) {
+    var y = rotation.y % 360;
+
+
+    return this.isAlmostEqual(y, 90, -270);
+};
+
+SoulPusher.prototype.isHalfRotated = function (rotation) {
+    var y = rotation.y % 360;
+    if (this.isAlmostEqual(y, 0))
+        return this.isAlmostEqual(rotation.x, 180) &&
+            this.isAlmostEqual(rotation.z, 180);
+
+    return this.isAlmostEqual(y, 180, -180);
+};
+
+SoulPusher.prototype.isThreeQuartersRotated = function (rotation) {
+    var y = rotation.y % 360;
+    // if (this.isAlmostEqual(y, 0))
+    //     return this.isAlmostEqual(rotation.x, 180) &&
+    //             this.isAlmostEqual(rotation.z, 180);
+
+    return this.isAlmostEqual(y, 270, -90);
+};
+
+SoulPusher.prototype.isAlmostEqual = function (x, a, b) {
     var e = 1;
     return Math.abs(x - a) < e || Math.abs(x - b) < e;
 };
